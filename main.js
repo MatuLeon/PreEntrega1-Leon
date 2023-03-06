6659647b7b9b59397c1b94c282435567edd7abae
Matias Leon <matiasleon2506@gmail.com>
2023-03-05 22:40:47 -0300

    Cambio idea de proyecto a alguiler de habitacion en hotel, falta completar funcion para sumar descuento con funciones grado alto

--- main.js
+++ main.js
@@ -1,45 +1,56 @@
-alert("Bienvenidos a Hotel LEON si esta buscando donde hosperdarse, esta en el lugar correcto!" + "" + "\n" + "Complete el formulario" )
+console.log("Bienvenidos a Hotel LEON si esta buscando donde hosperdarse, esta en el lugar correcto!" + "" + "\nComplete el formulario");
 
-let fullName = prompt("Ingrese su nombre completo")
-    while(fullName==""){
-        console.log ("Usted no ingreso ningun nombre")
-        fullName = prompt("Ingrese su nombre, por favor")
-    }
-
-let phoneNumber = Number(prompt("Ingrese su número de teléfono"));
-    while (phoneNumber==""){
-        alert ("Ingrese un número de teléfono, por favor")
-        phoneNumber= prompt("Ingrese su número")
-    }
-
-let peopleNumber = Number(prompt("Ingrese el número de familiares a hospedarse"));
-    if (peopleNumber === 1){
-        alert ("Tenemos espacio para usted!");
-    }else if (peopleNumber <=4 && peopleNumber > 1){
-        alert ("Tenemos espacio para usted y su familia!");
-    }else if (peopleNumber >=5 && peopleNumber <=8){
-        alert ("ULTIMA CABAÑA EXCLUSIVA PARA 8 PERSONAS MAXIMO, APROVECHE YA YA YA");
-    }else{
-        alert ("No hay cabañas disponibles, disculpe");
-    }
-
-    
-function Paymethod (){
-    let option1 = "transferencia"
-    let option2 = "tarjeta"
-    if (chosePaymethod === option1){
-        console.log(true + "" + " Usted eligio transferencia")
-        alert ("Si realiza la transferencia en las proximas 48 horas tiene un reintregro del 10%")
-    }else if (chosePaymethod === option2){
-        console.log(true + "" + " Usted eligio tarjeta")
-        alert ("Aceptamos todas las tarjetas y hasta 6 cuotas sin interes")
-    }else{
-        alert ("Elija un metodo de pago valido")
-    }
+function calcularIVA (arr){
+    let total;
+    arr.forEach ( producto =>{
+        total = producto.precio * 1.21
+    })
+    return total
 }
 
-let chosePaymethod =prompt("¿Quiere abonar con tarjeta o transferencia?").toLowerCase()
-Paymethod (chosePaymethod)
+function descuentoDebito (modoPago){
+    
+}
+const carritoCompra = []
+
+const listaHabitaciones = [ {id: 1, nombre: "Habitación individual", ubicación: "Frente, 2do piso", precio : 280.000, espacio: 1 },
+                            {id: 2, nombre: "Habitación individual", ubicación: "Contrafrente, planta baja", precio : 190.000 , espacio: 2 },
+                            {id: 3, nombre: "Habitación pareja", ubicación: "Frente, planta baja", precio : 350.000 , espacio: 2},
+                            {id: 4, nombre: "Habitación pareja", ubicación: "Frente, 3er piso", precio : 510.000 , espacio: 2},
+                            {id: 5, nombre: "Habitación familia", ubicación: "Contrafrente, 1er piso", precio : 580.000, espacio: 4 },
+                            {id: 6, nombre: "Habitación PREMIUM", ubicación: "Frente, 4to piso", precio : 780.000, espacio : 6},
+                            ]
 
 
+listaHabitaciones.forEach (el=>{
+    console.log(el.id)
+    console.log(el.nombre)
+    console.log(el.ubicación)
+    console.log(el.precio)
+    console.log(el.espacio)
+})
+
+
+
+let rta = prompt ("¿Esta buscando donde hospedarse?" + "\nResponda si o no").toLowerCase()
+
+    if (rta != "no"){
+        let espacio = Number (prompt ("¿Para cuantas personas?"));
+
+        if (listaHabitaciones.some( cantidad => cantidad.espacio >= espacio)){
+            carritoCompra.push(listaHabitaciones.find(cantidad => cantidad.espacio >= espacio))
+            alert("Aceptamos transferencias y tarjeta debito o credito")
+            let modoPago = prompt ("¿Paga con transferencia o tarjeta?").toUpperCase()
+            let option1 = "TRANSFERENCIA"
+            let option2 = "TARJETA"
+                if(modoPago == option1 ){
+                    
+                }
+        } else {
+            alert ("No contamos con habitaciones disponibles")
+        }
+    }
+
+
+    calcularIVA (carritoCompra);
 
