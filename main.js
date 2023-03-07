1adbe65e3729ce97f64a4f27cb116c5498710a9c
Matias Leon <matiasleon2506@gmail.com>
2023-03-06 21:14:24 -0300

    Falta completar descuento y recargo segun modo de pago

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ function calcularIVA (arr){
 
 const carritoCompra = []
 
-const listaHabitaciones = [ {id: 1, nombre: "Habitación individual", ubicación: "Frente, 2do piso", precio : 280.000, espacio: 1 },
-                            {id: 2, nombre: "Habitación individual", ubicación: "Contrafrente, planta baja", precio : 190.000 , espacio: 2 },
-                            {id: 3, nombre: "Habitación pareja", ubicación: "Frente, planta baja", precio : 350.000 , espacio: 2},
-                            {id: 4, nombre: "Habitación pareja", ubicación: "Frente, 3er piso", precio : 510.000 , espacio: 2},
-                            {id: 5, nombre: "Habitación familia", ubicación: "Contrafrente, 1er piso", precio : 580.000, espacio: 4 },
-                            {id: 6, nombre: "Habitación PREMIUM", ubicación: "Frente, 4to piso", precio : 780.000, espacio : 6},
+const listaHabitaciones = [ {id: 1, nombre: "Habitación individual", ubicación: "Frente, 2do piso", precio : 280000, espacio: 1 },
+                            {id: 2, nombre: "Habitación individual", ubicación: "Contrafrente, planta baja", precio : 190000 , espacio: 2 },
+                            {id: 3, nombre: "Habitación pareja", ubicación: "Frente, planta baja", precio : 350000 , espacio: 2},
+                            {id: 4, nombre: "Habitación pareja", ubicación: "Frente, 3er piso", precio : 510000 , espacio: 2},
+                            {id: 5, nombre: "Habitación familia", ubicación: "Contrafrente, 1er piso", precio : 580000, espacio: 4 },
+                            {id: 6, nombre: "Habitación PREMIUM", ubicación: "Frente, 4to piso", precio : 780000, espacio : 6},
                             ]
 
 
@@ -42,26 +42,6 @@ console.log (rta + "Entramos al if")
         if (listaHabitaciones.some( cantidad => cantidad.espacio >= espacio)){
             carritoCompra.push(listaHabitaciones.find(cantidad => cantidad.espacio >= espacio));
             console.log (carritoCompra);
-            alert("Aceptamos transferencias y tarjeta")
-            let modoPago = prompt ("¿Paga con transferencia o tarjeta?").toUpperCase()
-            let option1 = "TRANSFERENCIA"
-            let option2 = "TARJETA"
-            console.log (modoPago)
-                if(modoPago == option1 && modoPago != ""){
-                    function descuento (total , descuento){
-                        let resultado = ""
-                        resultado = total % descuento
-                        return resultado;
-                    }
-                }else if(modoPago == "TARJETA"){
-                    function recargo (total, descuento){
-                        let resultado = ""
-                        resultado = total % descuento
-                        console.log (resultado)
-                        return resultado
-                    }
-                }
-
         } else {
             alert ("No contamos con habitaciones disponibles")
         }
@@ -69,5 +49,22 @@ console.log (rta + "Entramos al if")
         alert ( persona + " Sentite libre de chusmear nuestras ofertas")
     }
 
+    alert("Aceptamos transferencias y tarjeta")
+    let modoPago = prompt ("¿Paga con transferencia o tarjeta?").toUpperCase()
+    let option1 = "TRANSFERENCIA"
+    let option2 = "TARJETA"
+    console.log (modoPago)
+        if(modoPago == option1 && modoPago != ""){
+            let aPagar = carritoCompra.precio % 15
+            console.log (aPagar)
+
+        }else if(modoPago == "TARJETA"){
+            let aPagar = Number (carritoCompra.find(precio => precio.precio) * 13)
+            console.log (aPagar)
+        }
+
     calcularIVA (carritoCompra);
+
+    console.log(carritoCompra)
+
 
