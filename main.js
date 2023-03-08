12cbd8358452e78ebe611dc317825e3c85158a4d
Matias Leon <matiasleon2506@gmail.com>
2023-03-07 22:40:53 -0300

    arreglar modo de pago y cuenta

--- main.js
+++ main.js
@@ -12,12 +12,12 @@ function calcularIVA (arr){
 const carritoCompra = []
 
 const listaHabitaciones = [ {id: 1, nombre: "Habitación individual", ubicación: "Frente, 2do piso", precio : 280000, espacio: 1 },
-                            {id: 2, nombre: "Habitación individual", ubicación: "Contrafrente, planta baja", precio : 190000 , espacio: 2 },
-                            {id: 3, nombre: "Habitación pareja", ubicación: "Frente, planta baja", precio : 350000 , espacio: 2},
-                            {id: 4, nombre: "Habitación pareja", ubicación: "Frente, 3er piso", precio : 510000 , espacio: 2},
-                            {id: 5, nombre: "Habitación familia", ubicación: "Contrafrente, 1er piso", precio : 580000, espacio: 4 },
-                            {id: 6, nombre: "Habitación PREMIUM", ubicación: "Frente, 4to piso", precio : 780000, espacio : 6},
-                            ]
+                            {id: 2, nombre: "Habitación pareja", ubicación: "Contrafrente, planta baja", precio : 190000 , espacio: 2 },
+                            {id: 3, nombre: "Habitación para tres", ubicación: "Frente, planta baja", precio : 350000 , espacio: 3},
+                            {id: 4, nombre: "Habitación cuartetera", ubicación: "Frente, 3er piso", precio : 510000 , espacio: 4},
+                            {id: 5, nombre: "Habitación 5 de copas", ubicación: "Contrafrente, 1er piso", precio : 580000, espacio: 5 },
+                            {id: 6, nombre: "Habitación Fiesta", ubicación: "Frente, 4to piso", precio : 780000, espacio : 6},
+                            ];
 
 
 listaHabitaciones.forEach (el=>{
@@ -42,6 +42,19 @@ console.log (rta + "Entramos al if")
         if (listaHabitaciones.some( cantidad => cantidad.espacio >= espacio)){
             carritoCompra.push(listaHabitaciones.find(cantidad => cantidad.espacio >= espacio));
             console.log (carritoCompra);
+            alert("Aceptamos transferencias y tarjeta")
+            let modoPago = prompt ("¿Paga con transferencia o tarjeta?").toUpperCase()
+            let option1 = "TRANSFERENCIA"
+            let option2 = "TARJETA"
+            console.log (modoPago)
+                if(modoPago == option1 && modoPago != ""){
+                    let aPagar = carritoCompra.precio.value
+                    console.log(aPagar)
+        
+                }else if(modoPago == "TARJETA"){
+                    let aPagar = carritoCompra.indexOf("precio")* 13
+                    console.log (aPagar)
+                }
         } else {
             alert ("No contamos con habitaciones disponibles")
         }
@@ -49,22 +62,10 @@ console.log (rta + "Entramos al if")
         alert ( persona + " Sentite libre de chusmear nuestras ofertas")
     }
 
-    alert("Aceptamos transferencias y tarjeta")
-    let modoPago = prompt ("¿Paga con transferencia o tarjeta?").toUpperCase()
-    let option1 = "TRANSFERENCIA"
-    let option2 = "TARJETA"
-    console.log (modoPago)
-        if(modoPago == option1 && modoPago != ""){
-            let aPagar = carritoCompra.precio % 15
-            console.log (aPagar)
 
-        }else if(modoPago == "TARJETA"){
-            let aPagar = Number (carritoCompra.find(precio => precio.precio) * 13)
-            console.log (aPagar)
-        }
 
     calcularIVA (carritoCompra);
 
-    console.log(carritoCompra)
+
 
 
