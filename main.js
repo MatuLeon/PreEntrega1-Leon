dcab7c9232fd11f9cdf35e4f4829898d900dda43
Matias Leon <matiasleon2506@gmail.com>
2023-03-22 00:09:27 -0300

    agrego storage y json

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-console.log("Bienvenidos a Hotel LEON si esta buscando donde hosperdarse, esta en el lugar correcto!" + "" + "\nComplete el formulario");
 
-/*------------------ FUNCIONES ------------ */
+/*Función que calcula el iva*/ 
 
 function calcularTotal (arr){
     let resultado = 0;
@@ -10,92 +9,95 @@ function calcularTotal (arr){
     return resultado
 }
 
-/*LOCAL STORAGE*/
-
-class Habitaciones{
-    constructor(id, nombre, precio, espacio){
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.espacio = espacio;
-    }
-}
-
-const listaHabitaciones = [new Habitaciones ]
 
 
-const carritoCompra = []
-
-/*const listaHabitaciones = [ {id: 1, nombre: "Habitación individual", ubicación: "Frente, 2do piso", precio : 280000, espacio: 1 },
-                            {id: 2, nombre: "Habitación pareja", ubicación: "Contrafrente, planta baja", precio : 190000 , espacio: 2 },
-                            {id: 3, nombre: "Habitación para tres", ubicación: "Frente, planta baja", precio : 350000 , espacio: 3},
-                            {id: 4, nombre: "Habitación cuartetera", ubicación: "Frente, 3er piso", precio : 510000 , espacio: 4},
-                            {id: 5, nombre: "Habitación 5 de copas", ubicación: "Contrafrente, 1er piso", precio : 580000, espacio: 5 },
-                            {id: 6, nombre: "Habitación Fiesta", ubicación: "Frente, 4to piso", precio : 780000, espacio : 6},
-                            ];
-*/
-
-let persona = prompt ("Hola y bienvenido/a, ingrese su nombre por favor")
-    while (persona == "" || persona.length <= 2){
-        alert ("Dato no valido")
-        persona = prompt("Hola y bienvenido, ingrese su nombre por favor");
-    }
-
-let rta = prompt ( "Hola " + persona + " ¿Esta buscando donde hospedarse?" + "\nResponda si o no").toLowerCase()
-console.log (rta + "Entramos al if")
-
-    if (rta != "no"){
-        
-        let espacio = Number (prompt ("¿Para cuantas personas?"));
-
-        if (listaHabitaciones.some( cantidad => cantidad.espacio >= espacio)){
-            carritoCompra.push(listaHabitaciones.find(cantidad => cantidad.espacio >= espacio));
-            console.log (carritoCompra);
-
-            alert("Aceptamos transferencias y tarjeta")
-
-            let modoPago = prompt ("¿Paga con transferencia o tarjeta?").toUpperCase()
-
-            let option1 = "TRANSFERENCIA"
-            let option2 = "TARJETA"
-            console.log (modoPago)
 
 
-                if(modoPago == option1 && modoPago != ""){
+//Array con productos
+const room_list = [
+    { id: 1, nombre: "Habitación individual", ubicación: "Planta baja", precio: 16000, espacio: 1 , img: "https://http2.mlstatic.com/D_NQ_NP_655385-MLA25685746798_062017-W.webp"},
+    { id: 2, nombre: "Habitación individual", ubicación: "Segundo piso", precio: 23000, espacio: 1, img: "https://funko.com/dw/image/v2/BGTS_PRD/on/demandware.static/-/Sites-funko-master-catalog/default/dwc5b8a303/images/funko/65654-3.png?sw=800&sh=800" },
+    { id: 3, nombre: "Habitación individual", ubicación: "Primer piso", precio: 21000, espacio: 1 },
+    { id: 4, nombre: "Habitación doble", ubicación: "Segundo piso", precio: 29000, espacio: 2 },
+    { id: 5, nombre: "Habitación matrimonial", ubicación: "Tercer piso", precio: 35000, espacio: 2 },
+    { id: 6, nombre: "Habitación para tres", ubicación: "Planta baja", precio: 32000, espacio: 3 },
+    { id: 7, nombre: "Habitación cuartetera", ubicación: "Tercer piso", precio: 51000, espacio: 4 },
+    { id: 8, nombre: "Habitación 5 de copas", ubicación: "Cuarto piso", precio: 58000, espacio: 5 },
+    { id: 9, nombre: "Habitación Fiesta", ubicación: "Quinto piso", precio: 78000, espacio: 6 },
 
-                    let aPagar = carritoCompra[0].precio -( carritoCompra[0].precio * 0.15);
-                    console.log ("Con el descuento del 15% le queda en $" + Math.round(aPagar))
-        
-                }else if(modoPago == "TARJETA"){
+]
+
+//Guardamos el array en JSON en localstorage
+const roomJSON = JSON.stringify(room_list)
+
+localStorage.setItem ("room_list", roomJSON)
+
+let roomJS = JSON.parse(localStorage.getItem("room_list")) || []
 
 
-                    let aPagar = carritoCompra[0].precio + (carritoCompra[0].precio* 0.13);
-                    console.log ("Con el recargo por tarjeta le queda en $" + Math.round(aPagar))
-                }else{
+
+const card_shopping = []
+
+const container_cardShop = document.getElementById("container_cardShop")
+
+const container_rooms = document.getElementById("container_rooms")
+
+//Recorro lista para crear un div por cada objeto
+
+room_list.forEach(room => {
+
+    container_rooms.innerHTML += `
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="${room.img}">
+        <div class="card-body">
+            <h5 class="card-title">${room.nombre}</h5>
+            <p class="card-text">${room.ubicación}
+            </p>
+            <p class="card-text">${room.precio}
+            </p>
+            <a href="#" class="btn btn-primary" id="room${room.id}">Reservar</a>
+        </div>
+    </div>`
+
+});
 
 
-                    alert ("Ingrese un modo de pago o presione 'ESC'");
-                    modoPago=prompt ("¿Paga con transferencia o tarjeta?").toUpperCase();
-                }
-        } else {
+room_list.forEach(room => {
+    const reserve = document.getElementById(`room${room.id}`)
+    reserve.addEventListener("click", ()=>{
 
+        card_shopping.push(room)
+
+        //Limpiamos el carrito
+        container_cardShop.innerHTML = ""
+
+        //Agregamos al carrito
+        card_shopping.forEach (room=>{
+
+            container_cardShop.innerHTML += 
+            `
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="..." class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h5 class="card-title">${room.nombre}</h5>
+                        <p class="card-text">${room.ubicación}</p>
+                        <p class="card-text"><small class="text-muted">Precio: $ ${room.precio}</small></p>
+                        <p class="card-text"><small class="text-muted">Con IVA : $ ${calcularTotal (card_shopping)}</small></p>
+                        <input type="number" id="dias" placeholder="Cuantos días quiere quedarse">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
             
-            alert ("No contamos con habitaciones disponibles")
-        }
-    }else {
-        alert ( persona + " Sentite libre de chusmear nuestras ofertas")
-    }
 
-
-
-    console.log("El total a pagar con impuesto IVA es de $" + calcularTotal (carritoCompra));
-
-
-    /*DOM */
-
-    const max_price = document.getElementById("max_price");
-
-    max_price.addEventListener ("change", ()=>{
-
+        })
+        let saveReserve = JSON.stringify (card_shopping)
+        localStorage.setItem ("reserva", saveReserve)
+        let returnReserve = localStorage.getItem (JSON.parse ("reserva"))
     })
-
+})
