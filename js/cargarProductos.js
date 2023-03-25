f78a041a1c153681bf83bbcee0617602c528967c
Matias Leon <matiasleon2506@gmail.com>
2023-03-25 20:10:21 -0300

    agregamos objetos y metodos para el e-commerse y storage

---
+++ js/cargarProductos.js
@@ -0,0 +1,43 @@
+let listaHabitaciones =[
+    { id: 1, nombre: "Habitación individual", ubicación: "Planta baja", precio: 16000, espacio: 1 , img: "https://http2.mlstatic.com/D_NQ_NP_655385-MLA25685746798_062017-W.webp", dias :1,alt :"habitación individual"  },
+    { id: 2, nombre: "Habitación individual", ubicación: "Segundo piso", precio: 23000, espacio: 1, img: "https://http2.mlstatic.com/D_NQ_NP_390111-MLA20460698853_102015-W.webp", dias :1,alt : "habitación individual"   },
+    { id: 3, nombre: "Habitación individual", ubicación: "Primer piso", precio: 21000, espacio: 1, img: "https://http2.mlstatic.com/D_NQ_NP_845727-MLA53571868357_022023-W.webp", dias :1,alt : "habitación individual"  },
+    { id: 4, nombre: "Habitación doble", ubicación: "Segundo piso", precio: 29000, espacio: 2, img: "https://http2.mlstatic.com/D_NQ_NP_943852-MLA44711490365_012021-W.webp", dias :1,alt : "habitación doble"   },
+    { id: 5, nombre: "Habitación matrimonial", ubicación: "Tercer piso", precio: 35000, espacio: 2, img:"https://http2.mlstatic.com/D_NQ_NP_871786-MLA47042878440_082021-W.webp", dias :1,alt : "habitación matrimonial"  },
+    { id: 6, nombre: "Habitación para tres", ubicación: "Planta baja", precio: 32000, espacio: 3, img:"https://http2.mlstatic.com/D_NQ_NP_782910-MLA52511361614_112022-W.webp", dias :1,alt : "habitación tres"  },
+    { id: 7, nombre: "Habitación cuartetera", ubicación: "Tercer piso", precio: 51000, espacio: 4, img:"https://http2.mlstatic.com/D_NQ_NP_897536-MLA52565385682_112022-W.webp", dias :1,alt : "habitación cuartetera"  },
+    { id: 8, nombre: "Habitación 5 de copas", ubicación: "Cuarto piso", precio: 58000, espacio: 5, img:"https://http2.mlstatic.com/D_NQ_NP_965023-MLA54453166171_032023-W.webp", dias :1,alt : "habitación 5"  },
+    { id: 9, nombre: "Habitación Fiesta", ubicación: "Quinto piso", precio: 78000, espacio: 6, img:"https://http2.mlstatic.com/D_NQ_NP_700099-MLA47590502580_092021-W.webp", dias :1 ,alt : "habitación premium" },
+
+]
+
+const listaHabitacionesJSON = JSON.stringify (listaHabitaciones)
+
+localStorage.setItem ("listaHabitaciones", listaHabitacionesJSON)
+
+/* let listaHabitaciones
+
+
+
+const form = document.getElementById ("formulario")
+
+form.addEventListener ("submit", (e)=>{
+
+    e.preventDefault()
+
+    const id = document.getElementById("id").value
+    const nombre = document.getElementById("nombre").value
+    const precio = document.getElementById("precio").value
+    const descripcion = document.getElementById("descripcion").value
+
+
+    listaHabitaciones.push ({id:id, nombre:nombre, precio:precio , descripcion : descripcion})
+
+    /*---Convertimos en string plano ---
+
+    const listaHabitacionesJSON = JSON.stringify(listaHabitaciones)
+
+    localStorage.setItem ("listaHabitaciones", listaHabitacionesJSON)
+
+    form.reset()
+})*/
--- js/main.js
+++ js/main.js
@@ -1,112 +1,127 @@
+class ProductoController{
+    constructor (){
+        this.listaHabitaciones = []
+    }
 
-/*Función que calcula el iva*/ 
 
-function calcularTotal (arr){
-    let resultado = 0;
-    arr.forEach ( element =>{
-        resultado += element.precio * 1.21
-    })
-    return resultado
+    levantarProductos(){
+        let obtenerListaJSON = localStorage.getItem ("listaHabitaciones")
+
+
+        if (obtenerListaJSON){
+            this.listaHabitaciones = JSON.parse (obtenerListaJSON)
+        }
+    }
+
+
+    mostrarDOM (contenedor_productos){
+        //limpio
+        contenedor_productos.innerHTML = ""
+
+        //muestro toda la lista
+        this.listaHabitaciones.forEach(habitacion => {
+            contenedor_productos.innerHTML += `
+            <div class="card" style="width: 18rem;">
+                <img src="${habitacion.img}" class="card-img-top" alt="${habitacion.alt}">
+                <div class="card-body">
+                    <h5 class="card-title">${habitacion.nombre}</h5>
+                    <p class="card-text">
+                        $${habitacion.precio}
+                    </p>
+                    <a href="#" class="btn btn-primary" id="habitacion${habitacion.id}">Reservar</a>
+                </div>
+            </div>`
+        });
+    }
+
 }
 
 
+class CarritoController {
+    constructor(){
+        this.listaCarrito = []
+    }
+
+    levantar (){
+        let obtenerListaJSON = localStorage.getItem ("listaCarrito")
+        if (obtenerListaJSON){
+            this.listaCarrito = JSON.parse (obtenerListaJSON)
+        }
+    }
 
 
-//Array con productos
-const room_list = [
-    { id: 1, nombre: "Habitación individual", ubicación: "Planta baja", precio: 16000, espacio: 1 , img: "https://http2.mlstatic.com/D_NQ_NP_655385-MLA25685746798_062017-W.webp"},
-    { id: 2, nombre: "Habitación individual", ubicación: "Segundo piso", precio: 23000, espacio: 1, img: "https://http2.mlstatic.com/D_NQ_NP_390111-MLA20460698853_102015-W.webp" },
-    { id: 3, nombre: "Habitación individual", ubicación: "Primer piso", precio: 21000, espacio: 1, img: "https://http2.mlstatic.com/D_NQ_NP_845727-MLA53571868357_022023-W.webp" },
-    { id: 4, nombre: "Habitación doble", ubicación: "Segundo piso", precio: 29000, espacio: 2, img: "https://http2.mlstatic.com/D_NQ_NP_943852-MLA44711490365_012021-W.webp"  },
-    { id: 5, nombre: "Habitación matrimonial", ubicación: "Tercer piso", precio: 35000, espacio: 2, img:"https://http2.mlstatic.com/D_NQ_NP_871786-MLA47042878440_082021-W.webp" },
-    { id: 6, nombre: "Habitación para tres", ubicación: "Planta baja", precio: 32000, espacio: 3, img:"https://http2.mlstatic.com/D_NQ_NP_782910-MLA52511361614_112022-W.webp" },
-    { id: 7, nombre: "Habitación cuartetera", ubicación: "Tercer piso", precio: 51000, espacio: 4, img:"https://http2.mlstatic.com/D_NQ_NP_897536-MLA52565385682_112022-W.webp" },
-    { id: 8, nombre: "Habitación 5 de copas", ubicación: "Cuarto piso", precio: 58000, espacio: 5, img:"https://http2.mlstatic.com/D_NQ_NP_965023-MLA54453166171_032023-W.webp" },
-    { id: 9, nombre: "Habitación Fiesta", ubicación: "Quinto piso", precio: 78000, espacio: 6, img:"https://http2.mlstatic.com/D_NQ_NP_700099-MLA47590502580_092021-W.webp" },
-
-]
-
-//Guardamos el array en JSON en localstorage
-const roomJSON = JSON.stringify(room_list)
-
-localStorage.setItem ("room_list", roomJSON)
-
-let roomJS = JSON.parse(localStorage.getItem("room_list")) || []
+    anadir (habitacion){
+        this.listaCarrito.push(habitacion)
+        
+        let carritoJSON = JSON.stringify(this.listaCarrito)
+        localStorage.setItem("listaCarrito", carritoJSON)
 
 
-
-const card_shopping = []
-
-const container_cardShop = document.getElementById("container_cardShop")
-
-const container_rooms = document.getElementById("container_rooms")
-
-//Recorro lista para crear un div por cada objeto
-
-room_list.forEach(room => {
-
-    container_rooms.innerHTML += `
-    <div class="card" style="width: 18rem;">
-        <img src="${room.img}" class="card-img-top" alt="">
-        <div class="card-body">
-            <h5 class="card-title">${room.nombre}</h5>
-            <p class="card-text">${room.ubicación}
-            </p>
-            <p class="card-text">$ ${room.precio}
-            </p>
-            <a href="#" class="btn btn-primary" id="room${room.id}">Reservar</a>
-        </div>
-    </div>`
-
-});
+    }
 
 
-room_list.forEach(room => {
-    const reserve = document.getElementById(`room${room.id}`)
-    reserve.addEventListener("click", ()=>{
+    mostrarDOM(contenedor_carrito){
 
-        card_shopping.push(room)
-
-        //Limpiamos el carrito
-        container_cardShop.innerHTML = ""
-
-        //Agregamos al carrito
-        card_shopping.forEach (room=>{
-
-            container_cardShop.innerHTML += 
-            `
+        //limpio contenedor
+        contenedor_carrito.innerHTML = ""
+        //muestro todo
+        this.listaCarrito.forEach (producto =>{
+            contenedor_carrito.innerHTML += `
             <div class="card mb-3" style="max-width: 540px;">
                 <div class="row g-0">
                     <div class="col-md-4">
-                        <img src="${room.img}" class="img-fluid rounded-start" alt="...">
+                    <img src="${producto.img}" class="img-fluid rounded-start" alt="...">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
-                        <h5 class="card-title">${room.nombre}</h5>
-                        <p class="card-text">${room.ubicación}</p>
-                        <p class="card-text">${room.ubicación}</p>
-                        <p class="card-text"><small class="text-muted">Precio: $ ${room.precio}</small></p>
-                        <input type="text" id="dias" placeholder="Cuantos días quiere quedarse">
-                        <p class="card-text"><small class="text-muted">Con IVA : $ ${calcularTotal (card_shopping)}</small></p>
+                            <h5 class="card-title">${producto.nombre}</h5>
+                            <p class="card-text">
+                                    ${producto.ubicación}
+                            </p>
+                            <p class="card-text">
+                                <small class="text-body-secondary">$${producto.precio}</small>
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
+    
             `
-            
+            })
+    }
+}
 
-        
-        })
-
-        let saveReserve = JSON.stringify (card_shopping)
-        localStorage.setItem ("reserva", saveReserve)
-        let returnReserve = JSON.parse(localStorage.getItem("reserva")) || []
-
-        let days = document.getElementById ("dias").value
-
-        
+//Objetos controladores
+const controladorProducto = new ProductoController ();
+const controladorCarrito = new CarritoController();
 
 
+//VERIFICAR STORAGE
+controladorProducto.levantarProductos();
+controladorCarrito.levantar();
+
+//DOM
+const contenedor_productos = document.getElementById ("contenedor_productos");
+
+const contenedor_carrito = document.getElementById ("contenedor_carrito");
+
+//APP JS
+
+/*---Recorremos el array para agregar html a cada habitación---------*/
+controladorProducto.mostrarDOM(contenedor_productos);
+controladorCarrito.mostrarDOM(contenedor_carrito);
+
+
+
+/*---Evento de click para sumar un producto al carrito--------- */
+
+controladorProducto.listaHabitaciones.forEach (habitacion =>{
+    const sumarCarrito = document.getElementById (`habitacion${habitacion.id}`)
+
+    sumarCarrito.addEventListener ("click", ()=>{
+
+        controladorCarrito.anadir(habitacion)
+        controladorCarrito.levantar
+        controladorCarrito.mostrarDOM(contenedor_carrito)
     })
-
 })
